a91d710787c1de72c03cbc3358f4a0c6a9a145b6
MicheleFamoso <michelefamoso@gmail.com>
2025-03-20 14:29:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cont = document.getElementById("cont");
+const cont = document.getElementById("col");
 
 const library = function () {
   fetch("https://striveschool-api.herokuapp.com/books", {})
@@ -7,7 +7,7 @@ const library = function () {
         console.log("Andata", response);
         return response.json();
       } else {
-        throw new Error("nooooooo");
+        throw new Error("noooooo");
       }
     })
     .then((data) => {
@@ -15,14 +15,14 @@ const library = function () {
 
       data.forEach((book) => {
         const col = document.createElement("div");
-        col.className = "col col-12 col-md-3 mt-5";
+        col.className = "col col-12 col-md-4 col-xl-3 mt-5";
         col.innerHTML = `
-             <div class="card" style="width: 18rem">
-              <img src="${book.img}" class="card-img-top" alt="..." />
+             <div class="card ">
+              <img src="${book.img}" class="card-img-top  " alt="..." />
               <div class="card-body">
-                <h5 class="card-title">${book.title}</h5>
-                <h5 class="card-title">${book.price}</h5>
-                <h5 class="card-title">${book.category}</h5>
+                <h5 class="card-title">Titolo - ${book.title}</h5>
+                <h5 class="card-title"> Prezzo - $${book.price}</h5>
+                <h5 class="card-title">Categoria - ${book.category}</h5>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div> 
